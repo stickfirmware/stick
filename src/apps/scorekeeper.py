deb6920c23ae618cc0d2ab69403c43e1716c4bd9
Kitki30 <kitki30@proton.me>
2025-07-27 19:28:27 +0200

    small refactor

--- src/apps/scorekeeper.py
+++ src/apps/scorekeeper.py
@@ -26,33 +26,33 @@ def run():
     
     print("Going into main loop")
     
-    team1 = 0
-    team2 = 0
+    team_1 = 0
+    team_2 = 0
     
     t1_nvs = nvs.get_int(app_storage, "team1")
     t2_nvs = nvs.get_int(app_storage, "team2")
     if t1_nvs != None:
-        team1 = t1_nvs
+        team_1 = t1_nvs
     else:
         nvs.set_int(app_storage, "team1", 0)
     if t2_nvs != None:
-        team2 = t2_nvs
+        team_2 = t2_nvs
     else:
         nvs.set_int(app_storage, "team2", 0)
     
     work = True
     while work == True:
-        render = menus.menu("Scorekeeper", [("Team 1: " + str(team1), 1), ("Team 2: " + str(team2), 2), ("Reset", 3), ("Close", 4)])
+        render = menus.menu("Scorekeeper", [("Team 1: " + str(team_1), 1), ("Team 2: " + str(team_2), 2), ("Reset", 3), ("Close", 4)])
         if render == 1:
-            team1 += 1
+            team_1 += 1
         elif render == 2:
-            team2 += 1
+            team_2 += 1
         elif render == 3:
-            team1 = 0
-            team2 = 0
+            team_1 = 0
+            team_2 = 0
         else:
             work = False
     
-    nvs.set_int(app_storage, "team1", team1)
-    nvs.set_int(app_storage, "team2", team2)
+    nvs.set_int(app_storage, "team1", team_1)
+    nvs.set_int(app_storage, "team2", team_2)
         
