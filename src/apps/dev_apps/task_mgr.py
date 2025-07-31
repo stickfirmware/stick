4923a829bfcd509c96188afb139f60dab4a4153b
Kitki30 <kitki30@proton.me>
2025-07-31 17:23:41 +0200

    Fix hardware info

--- src/apps/dev_apps/task_mgr.py
+++ src/apps/dev_apps/task_mgr.py
@@ -4,8 +4,8 @@ import modules.menus as menus
 
 def run():
         menu = []
-        menu.append("CPU freq: "(str(machine.freq() // 1000000) + "MHz", None))
-        menu.append("Ram free: "(str(gc.mem_free() / 1024) + "KB", None))
-        menu.append("Ram used: "(str(gc.mem_alloc() / 1024) + "KB", None))
+        menu.append("CPU freq: " + (str(machine.freq() // 1000000) + "MHz", None))
+        menu.append("Ram free: " + (str(gc.mem_free() / 1024) + "KB", None))
+        menu.append("Ram used: " + (str(gc.mem_alloc() / 1024) + "KB", None))
         menu.append(("Close", None))
         menus.menu("Hardware info", menu)
