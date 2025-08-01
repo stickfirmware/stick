d248f55a581dcfc81db4a1346a449e4ad9f5f639
Kitki30 <kitki30@proton.me>
2025-08-01 11:06:26 +0200

    More advanced RAM cleaning, fix typo

--- src/modules/ram_cleaner.py
+++ src/modules/ram_cleaner.py
@@ -34,7 +34,7 @@ _WHITELIST = {
         'modules.cardputer_kb',
         'modules.ram_cleaner',
         'modules.rtc',
-        'modules.mpu6886'
+        'modules.mpu6886',
         'fonts.def_8x8',
         'fonts.def_16x16',
         'fonts.def_16x32',
@@ -46,14 +46,26 @@ _WHITELIST = {
         'flashbdev',
     }
 
+def deep_clean_module(modname):
+    mod = sys.modules.get(modname)
+    if not mod:
+        return
+    for attr in dir(mod):
+        try:
+            setattr(mod, attr, None)
+        except:
+            pass
+    sys.modules.pop(modname, None)
+
 def clean():
     printer.log("Ram cleaner started")
     before = gc.mem_free()
 
-    current_keys = set(sys.modules.keys())
+    current_keys = list(sys.modules.keys())
     for k in current_keys:
-        if k not in _WHITELIST:
-            del sys.modules[k]
+        if k not in _WHITELIST and not k.startswith("uasyncio"):
+            printer.log(str(k))
+            deep_clean_module(k)
 
     gc.collect()
     after = gc.mem_free()
