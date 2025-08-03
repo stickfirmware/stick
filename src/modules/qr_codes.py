b419d9dbb2e94abb5a0ee4bd7360e6ee8d7be4ae
Kitki30 <kitki30@proton.me>
2025-08-03 21:22:42 +0200

    Better qr code ram saving

--- src/modules/qr_codes.py
+++ src/modules/qr_codes.py
@@ -1,5 +1,6 @@
-import framebuf
+import machine
 
+import modules.os_constants as osc
 import modules.uQR as uQR
 
 def make_qr(tft, data, xd, yd, size=1):
@@ -7,22 +8,16 @@ def make_qr(tft, data, xd, yd, size=1):
     render_qr(tft, qr, xd, yd, size)
 
 def render_qr(tft, qr, xd, yd, size=1):
-    qr_w = len(qr[0]) * size
-    qr_h = len(qr) * size
-
-    buf = bytearray(qr_w * qr_h * 2)
-    fb = framebuf.FrameBuffer(buf, qr_w, qr_h, framebuf.RGB565)
-
+    curr_freq = machine.freq()
+    machine.freq(osc.ULTRA_FREQ)
     for y, line in enumerate(qr):
-        base_y = y * size
         for x, pixel in enumerate(line):
             color = 0x0000 if pixel else 0xFFFF
-            base_x = x * size
-            if size == 1:
-                fb.pixel(base_x, base_y, color)
-            else:
-                for dy in range(size):
-                    for dx in range(size):
-                        fb.pixel(base_x + dx, base_y + dy, color)
+            px = xd + x * size
+            py = yd + y * size
 
-    tft.blit_buffer(buf, xd, yd, qr_w, qr_h)
+            if size == 1:
+                tft.pixel(px, py, color)
+            else:
+                tft.fill_rect(px, py, size, size, color)
+    machine.freq(curr_freq)
