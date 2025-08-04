696cc2285129aac94599a0efb5cd33c8e00e25ed
Kitki30 <kitki30@proton.me>
2025-08-04 13:43:50 +0200

    Fix wi-fi

--- src/modules/wifi_master.py
+++ src/modules/wifi_master.py
@@ -16,22 +16,33 @@ def nic_reset():
     nic.active(True)
     time.sleep(0.5)
 
+# Default NVS vars
+def set_defaults():
+    nvs.set_int(n_wifi, "wifimode", 3)
+    nvs.set_float(n_wifi, "txpower", 15.0)
+
 # Set pwr mode based on NVS
-def set_pwr_modes(pm_mode=None):
-    if pm_mode is None:
+def set_pwr_modes(pmm=None):
+    if pmm is None:
         pm_mode = nvs.get_int(n_wifi, "wifimode")
         tx_power = nvs.get_float(n_wifi, "txpower")
     else:
-        nvs.set_int(n_wifi, "wifimode", pm_mode)
-        tx_power = 20 - (pm_mode * 5)
+        nvs.set_int(n_wifi, "wifimode", pmm)
+        pm_mode = pmm
+        tx_power = 20 - (pmm * 5)
         nvs.set_float(n_wifi, "txpower", tx_power)
+    
+    if pm_mode is None:
+        set_defaults()
+        return set_pwr_modes(pmm)
 
     if pm_mode == 3:
         dynamic_pwr_save()
     else:
-        nic.config(pm=pm_mode)
-        if tx_power is not None:
-            nic.config(txpower=tx_power)
+        if nic.active():
+            nic.config(pm=pm_mode)
+            if tx_power is not None:
+                nic.config(txpower=tx_power)
 
 
 # Dynamic power saving based on rssi (Change tx power and pm modes)
