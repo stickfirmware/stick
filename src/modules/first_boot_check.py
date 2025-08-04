2c2b19a87465f357ecc5e3f441035f5e2aa940b5
Kitki30 <kitki30@proton.me>
2025-08-04 12:59:57 +0200

    Change first boot color to more good looking

--- src/modules/first_boot_check.py
+++ src/modules/first_boot_check.py
@@ -13,23 +13,22 @@ def check(tft):
     n_boot = esp32.NVS("boot")
     if nvs.get_int(n_boot, "firstBoot") == None:
         n_settings = esp32.NVS("settings")
-        n_wifi = esp32.NVS("wifi")
         printer.log("Devices first boot, configuring NVS.")
-        tft.fill(7003)
-        tft.text(f16x16, "Stick firmware",0,0,65535,7003)
-        tft.text(f8x8, "First boot configuration!",0,16,65535,7003)
-        tft.text(f8x8, "Please wait...",0,24,65535,7003)
+        tft.fill(0)
+        tft.text(f16x16, "Stick firmware",0,0,65535,0)
+        tft.text(f8x8, "First boot configuration!",0,16,65535,0)
+        tft.text(f8x8, "Please wait...",0,24,65535,0)
     
         # Boot config
         printer.log("Configuring 'boot' NVS")
         nvs.set_int(n_boot, "firstBoot", 1)
         printer.log("boot:firstBoot:1")
-        tft.text(f8x8, "boot:firstBoot:1",0,34,65535,7003)
+        tft.text(f8x8, "boot:firstBoot:1",0,34,65535,0)
         
         import apps.oobe as oobe
         oobe.createUserFolder()
         oobe.createConfig()
     
         printer.log("Doing soft reset")
-        tft.text(f8x8, "Doing soft reset. Bye!",0,127,65535,7003)
+        tft.text(f8x8, "Doing soft reset. Bye!",0,127,65535,0)
         machine.soft_reset()
