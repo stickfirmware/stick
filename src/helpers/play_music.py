469f3820ae1e45ef8fbe31952c8dca7917360f2d
Kitki30 <kitki30@proton.me>
2025-08-21 10:26:17 +0200

    Remove music player from menu as its in explorer

--- src/helpers/play_music.py
+++ src/helpers/play_music.py
@@ -1,3 +1,9 @@
+import modules.os_constants as osc
+import modules.popup as popup
+
 def open_file(path):
+    if not osc.HAS_SPEAKER:
+        popup.show("Speaker was not detected in your device, music player is not supported.", "Error", 10)
+        return
     import apps.player as player
     player.play(path)
