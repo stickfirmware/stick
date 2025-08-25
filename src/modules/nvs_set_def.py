f5c11f0d6912e29d476fe5718bb3fa3fea843fd8
Kitki30 <kitki30@proton.me>
2025-08-25 15:06:33 +0200

    fix wifi

--- src/modules/nvs_set_def.py
+++ src/modules/nvs_set_def.py
@@ -6,6 +6,7 @@ import modules.nvs as nvs
 
 n_locks = cache.get_nvs('locks')
 n_settings = cache.get_nvs('settings')
+n_wifi = cache.get_nvs('wifi')
 
 def ints(namespace, key, defvar, cachename):
     var = nvs.get_int(namespace, key)
@@ -38,12 +39,18 @@ def run():
 
     # Buzz vol
     floats(n_settings, 'volume', 0.5, 'volume')
+    
+    # Wifi conf
+    float(n_wifi, 'conf', 0.0, 'conf')
 
     # Auto rotation
     ints(n_settings, 'autorotate', 1, 'arotate')
 
     # Pwr saving
     ints(n_settings, 'allowsaving', 1, 'pwrsave')
+    
+    # Metrics
+    ints(n_settings, 'allow_metrics', 0, 'metrics')
     
     # Language settings
     strings(n_settings, 'lang', "en", 'lang')
