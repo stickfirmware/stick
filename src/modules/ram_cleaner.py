9de083913bf9f3692fc9374689480c7e9b1b41dc
Kitki30 <kitki30@proton.me>
2025-09-11 07:11:29 +0200

    Async buzzer

--- src/modules/ram_cleaner.py
+++ src/modules/ram_cleaner.py
@@ -14,6 +14,7 @@ _WHITELIST = {
         'neopixel',
         'bitmaps',
         'scripts',
+        'asyncio',
         'scripts.checkbattery',
         'modules',
         'modules.battery_check',
