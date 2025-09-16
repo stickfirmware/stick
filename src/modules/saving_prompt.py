a0557f4eb99c277c3f9673c8ea00ff7d20beaa20
Kitki30 <kitki30@proton.me>
2025-09-16 12:28:04 +0200

    Add saving prompt handler

---
+++ src/modules/saving_prompt.py
@@ -0,0 +1,19 @@
+import modules.io_manager as io_man
+
+import fonts.def_8x8 as f8x8
+
+def show_saving_prompt(clear = False):
+    """
+    Shows saving prompt on display
+    
+    Args:
+        clean (bool): If True, clears display before showing prompt
+    """
+    
+    tft = io_man.get('tft')
+    if clear:
+        tft.fill(0)
+        
+    # TODO: Translate
+    tft.text(f8x8, "Saving...", 0,0,65535)
+    tft.text(f8x8, "Do not turn off!!!", 0,8,65535)
