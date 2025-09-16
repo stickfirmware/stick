76d3faa8badb89a9de57623f6d3583ab7dcf5258
Kitki30 <kitki30@proton.me>
2025-09-16 12:29:36 +0200

    Add neopixel to save prompt

--- src/modules/saving_prompt.py
+++ src/modules/saving_prompt.py
@@ -1,4 +1,5 @@
 import modules.io_manager as io_man
+import modules.os_constants as osc
 
 import fonts.def_8x8 as f8x8
 
@@ -17,3 +18,8 @@ def show_saving_prompt(clear = False):
     # TODO: Translate
     tft.text(f8x8, "Saving...", 0,0,65535)
     tft.text(f8x8, "Do not turn off!!!", 0,8,65535)
+    
+    # Make neopixel red
+    if osc.HAS_NEOPIXEL:
+        import modules.neopixel_anims as np
+        np.static((255,0,0))
