8b254b0540dab665a9802f3140a52998a55eb182
Kitki30 <kitki30@proton.me>
2025-10-05 10:41:47 +0200

    saving_prompt: Translated saving message

--- src/modules/saving_prompt.py
+++ src/modules/saving_prompt.py
@@ -1,6 +1,7 @@
 import fonts.def_8x8 as f8x8
 import modules.io_manager as io_man
 import modules.os_constants as osc
+from modules.translate import get as l_get
 
 
 def show_saving_prompt(clear = False):
@@ -14,10 +15,9 @@ def show_saving_prompt(clear = False):
     tft = io_man.get('tft')
     if clear:
         tft.fill(0)
-        
-    # TODO: Translate
-    tft.text(f8x8, "Saving...", 0,0,65535)
-    tft.text(f8x8, "Do not turn off!!!", 0,8,65535)
+
+    tft.text(f8x8, l_get("saving_prompt.saving"), 0,0,65535)
+    tft.text(f8x8, l_get("saving_prompt.do_no_turn_off"), 0,8,65535)
     
     # Make neopixel red
     if osc.HAS_NEOPIXEL:
