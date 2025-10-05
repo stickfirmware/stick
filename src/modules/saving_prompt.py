f629e9ab33b2c9f653d72903efe8071b3fa7a209
Kitki30 <kitki30@proton.me>
2025-10-05 11:32:07 +0200

    saving_prompt: Delete neopixel

--- src/modules/saving_prompt.py
+++ src/modules/saving_prompt.py
@@ -1,6 +1,5 @@
 import fonts.def_8x8 as f8x8
 import modules.io_manager as io_man
-import modules.os_constants as osc
 from modules.translate import get as l_get
 
 
@@ -18,8 +17,3 @@ def show_saving_prompt(clear = False):
 
     tft.text(f8x8, l_get("saving_prompt.saving"), 0,0,65535)
     tft.text(f8x8, l_get("saving_prompt.do_no_turn_off"), 0,8,65535)
-    
-    # Make neopixel red
-    if osc.HAS_NEOPIXEL:
-        import modules.neopixel_anims as np
-        np.static(255,0,0)
