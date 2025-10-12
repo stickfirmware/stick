bcd496fb90a8d972d84c7c8c22f82f916a428e96
Kitki30 <kitki30@proton.me>
2025-10-12 11:58:38 +0200

    recovery: Add dev menu shortcut to recovery

--- src/recovery/recovery.py
+++ src/recovery/recovery.py
@@ -26,8 +26,15 @@ io_man.set('button_b', button_b)
 io_man.set('button_c', button_c)
 io_man.set('tft', tft)
 
+def load_translations():
+    popup.show("To run this app properly, I will need to load english translation. Make sure it is working, or else you can expect crashes/freezes.")
+    import modules.translate as translate
+    translate.load("en")
+
 while True:
-    render = menus.menu("Recovery menu", [("Factory reset", 1), ("File explorer", 2), ("Toggle dev apps", 3), ("Disable custom SD", 4), ("Mount sd card", 5), ("Reboot", 13)])
+    render = menus.menu("Recovery menu", [("Factory reset", 1), ("File explorer", 2), ("Toggle dev apps", 3), ("Open dev apps", 6), ("Disable custom SD", 4), ("Mount sd card", 5), ("Reboot", 13)])
+    
+    # Factory
     if render == 1:
         confirm_reset = menus.menu("Reset all settings?", [("No", None), ("Yes", 1)])
         if confirm_reset == 1:
@@ -36,14 +43,16 @@ while True:
                 n_updates = cache.get_nvs('updates')
                 nvs.set_int(n_updates, "factory", 1)
                 machine.reset()
+                
+    # File explorer
     elif render == 2:
-        popup.show("To run file explorer properly, I will need to load english translation. Make sure it is working, or else you can expect crashes/freezes.")
-        import modules.translate as translate
-        translate.load("en")
+        load_translations()
         import modules.file_explorer as a_fe
         a_fe.run()
         decache('modules.file_explorer')
         del a_fe
+        
+    # Toggle developer
     elif render == 3:
         n_settings = cache.get_nvs('settings')
         dev_settings = nvs.get_int(n_settings, "dev_apps")
@@ -51,9 +60,13 @@ while True:
             nvs.set_int(n_settings, "dev_apps", 0)
         else:
             nvs.set_int(n_settings, "dev_apps", 1)
+            
+    # Disable SD overwrite
     elif render == 4:
         n_settings = cache.get_nvs('settings')
         nvs.set_int(n_settings, "sd_overwrite", 0)
+        
+    # Mount SD Card
     elif render == 5:
         try:
             import modules.sdcard as sd
@@ -61,5 +74,17 @@ while True:
             sd.mount()
         except Exception as e:
             popup.show(f"Error mounting SD card.\nDoes your device have sdcard slot?\nCustom sd card is not currently supported here!\nError msg:\n{str(e)}", "Error", 10)
+            
+    # Open dev apps
+    elif render == 6:
+        load_translations()
+        import modules.cache as cache
+        cache.precache()
+        import apps.dev_apps.dev_menu as dev
+        dev.run()
+        decache('apps.dev_apps.dev_menu')
+        del dev
+    
+    # Reboot
     elif render == 13:
         machine.reset()
