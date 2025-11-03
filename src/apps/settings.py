a9258a493fa68b96c7e5a45ef28d2eaefc02eb61
Kitki30 <kitki30@proton.me>
2025-11-03 16:41:11 +0100

    Backup settings

--- src/apps/settings.py
+++ src/apps/settings.py
@@ -56,6 +56,7 @@ def run():
                             (l_get("apps.settings.menu1.language"), 11),
                             (l_get("apps.settings.menu1.about"), 8),
                             (l_get("apps.settings.menu1.factory"), 9),
+                            (l_get("apps.settings.menu1.backups"), 10),
                             (l_get("apps.settings.menu1.show_guides_again"), 12),
                             (l_get("menus.menu_close"), None)]) # ("Account", 10),
         
@@ -569,6 +570,116 @@ def run():
                 if confirm_reset == 1:
                     nvs.set_int(n_updates, "factory", 1) # Set NVS bootloader entry
                     machine.soft_reset() # Reboot
+        
+        # Backups
+        elif menu1 == 10:
+            import modules.backups as backups
+            import modules.files as files
+            backup_menu = menus.menu(l_get("apps.settings.backups.title"), [
+                (l_get("apps.settings.backups.create_backup"), 1),
+                (l_get("apps.settings.backups.restore_backup"), 2),
+                (l_get("menus.menu_close"), None)
+            ])
+            
+            # Create
+            if backup_menu == 1:
+                location = menus.menu(l_get("apps.settings.backups.backup_to"), [
+                    (l_get("apps.settings.backups.internal_storage"), f"/usr/backups/backup_{time.time()}.bak"),
+                    (l_get("apps.settings.backups.sd_card"), f"/sd/backups/backup_{time.time()}.bak"),
+                    (l_get("menus.menu_close"), None)
+                ])
+                
+                if location is None:
+                    continue
+                
+                # Boost freq for faster backup
+                ps.boost_allowing_state(True)
+                ps.boost_clock()
+                
+                # Create folders
+                files.mkdir_recursive("/usr/backups")
+                if "sd" in os.listdir("/"):
+                    files.mkdir_recursive("/sd/backups")
+                
+                # Backup screen
+                tft.fill(0)
+                tft.text(f8x8, l_get("apps.settings.backups.creating_backup"), 0, 0, 65535)
+                
+                result_ok = False
+                
+                try:
+                    result = backups.create_backup(location)
+                    result_ok = result
+                except Exception as e:
+                    printer.log(f"Backup creation error: {e}", log_levels.ERROR)
+                    result_ok = False
+
+                ps.boost_allowing_state(False) # Disable boosts
+                ps.loop()
+                    
+                if result_ok:
+                    popup.show(l_get("apps.settings.backups.backup_created").replace("%path%", location), l_get("popups.info"), 10)
+                    continue
+                else:
+                    popup.show(l_get("apps.settings.backups.error_occurred"), l_get("crashes.error"), 10)
+                    continue
+            
+            # Restore
+            elif backup_menu == 2:
+                import modules.file_explorer as file_explorer
+                location = file_explorer.run(True)
+                if location is None:
+                    continue
+                
+                # Confirm restore
+                confirm_restore = menus.menu(l_get("apps.settings.backups.restore_ask"), [
+                    (l_get("menus.no"), None),
+                    (l_get("menus.yes"), 1)
+                ])
+                
+                if confirm_restore is None:
+                    continue
+                
+                # Restore screen
+                tft.fill(0)
+                tft.text(f8x8, l_get("apps.settings.backups.restoring_backup"), 0, 0, 65535)
+                
+                # Backup to temp in case of failure
+                ps.boost_allowing_state(True)
+                ps.boost_clock()
+                temp_backup_path = f"/temp/backups/temp_restore_backup_{time.time()}.bak"
+                files.mkdir_recursive("/temp/backups")
+                backup_ok = backups.backup_all(temp_backup_path)
+                if not backup_ok:
+                    ps.boost_allowing_state(False)
+                    popup.show(l_get("apps.settings.backups.restore_temp_error"), l_get("crashes.error"), 10)
+                    continue
+                
+                ps.boost_allowing_state(False) # Disable boosts
+                ps.loop()
+                
+                # Restore
+                result_ok = False
+                try:
+                    result_ok = backups.restore_backup(location)
+                except Exception as e:
+                    printer.log(f"Backup restore error: {e}", log_levels.ERROR)
+                    result_ok = False
+                    
+                if result_ok:
+                    popup.show(l_get("apps.settings.backups.backup_restored"), l_get("popups.info"), 10)
+                    # Restart ask
+                    restart_ask = menus.menu(l_get("apps.settings.backups.restart_now"), [
+                        (l_get("menus.no"), None),
+                        (l_get("menus.yes"), 1)
+                    ])
+                    if restart_ask == 1:
+                        import modules.power as power
+                        power.soft_reboot()
+                    continue
+                else:
+                    popup.show(l_get("apps.settings.backups.error_occurred"), l_get("crashes.error"), 10)
+                    continue
         else:
             work = False
             
