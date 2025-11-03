4b02f3379c7ac1ab9c5dbb95c5c2ee280b6fd42f
Kitki30 <kitki30@proton.me>
2025-11-03 07:06:05 +0100

    backups: Backup system part 2
    Added restore_all, fixed some errors

--- src/modules/backups.py
+++ src/modules/backups.py
@@ -11,6 +11,18 @@ import modules.nvs as nvs
 import modules.os_constants as osc
 import version
 
+class CannotOpenBackup(Exception):
+    pass
+
+class UnknownVariableType(Exception):
+    pass
+
+class DeviceCompatibilityError(Exception):
+    pass
+
+class KeylistVersionMismatch(Exception):
+    pass
+
 def backup_all(filename: str):
     """
     Backups all known system values
@@ -51,7 +63,7 @@ def backup_all(filename: str):
         elif args[2] == "string":
             value = nvs.get_string(namespace, args[1])
         else:
-            return False
+            raise UnknownVariableType(f"Unknown variable type: {args[1]}")
 
         backup_dict["keys"][key] = value
     
@@ -59,3 +71,62 @@ def backup_all(filename: str):
     decache("modules.backup_keylist")
 
     return json.write(filename, backup_dict)
+
+def restore_all(filename: str, verify: bool = True):
+    """
+    Restores NVS values from backup
+
+    Note:
+        This will only restore values that match keys in keylist!
+    
+    Args:
+        filename (str): Path where to read backup file
+        verify (bool, optional): If true (default) it will check backup compatibility with device and OS
+        
+    Returns:
+        bool: True if success, False otherwise
+    """
+
+    backup_dict = json.read(filename)
+    if backup_dict is None:
+        raise CannotOpenBackup(f"Cannot open backup, {filename}!")
+
+    # Check if device is the same
+    backup_device = backup_dict["manifest"]["device_name"]
+    release_device = osc.RELEASE_NAME
+    if verify and backup_device != release_device:
+        raise DeviceCompatibilityError(f"Backup device {backup_device}, is different than current device {release_device}!")
+    del release_device, backup_device
+    
+    # Check if keylist in backup matches system keylist
+    import modules.backup_keylist as backup_keys
+    list_ver_system = backup_keys.LIST_VERSION
+    list_ver_backup = backup_dict["manifest"]["backup_version"]
+
+    if verify and list_ver_system != list_ver_backup:
+        raise KeylistVersionMismatch(f"Backup keylist version {list_ver_backup}, is different than system {list_ver_system}!")
+    del list_ver_backup, list_ver_system
+
+    # Backup keys specified in keylist
+    for key in backup_dict["keys"]:
+        # Skip unknown keys to prevent erasing app data with malicious backups.
+        if key not in backup_keys.KEYS:
+            continue
+
+        args = key.split(";")
+        namespace = cache.get_nvs(args[0])
+
+        value = backup_dict["keys"][key]
+        if isinstance(value, int):
+            nvs.set_int(namespace, args[1], value)
+        elif isinstance(value, float):
+            nvs.set_float(namespace, args[1], value)
+        elif isinstance(value, str):
+            nvs.set_string(namespace, args[1], value)
+        else:
+            raise UnknownVariableType(f"Unknown variable type: {type(value)}")
+    
+    del backup_keys
+    decache("modules.backup_keylist")
+
+    return True
